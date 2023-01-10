349dd2643ad74cee9bd57cf9f270af57cf776be6
VladimirRanski <ranski.vladimir@gmail.com>
2023-01-11 02:08:45 +0300

    feat: function decode

--- src/index.js
+++ src/index.js
@@ -38,7 +38,32 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let res = '';
+    const arrBin = []
+
+    for (let i = 0; i < expr.length; i += 10) {
+        arrBin.push(expr.substring(i, i + 10))
+    }
+
+    for (let i = 0; i < arrBin.length; i++){
+        let morse = "";
+        for (let j = 0; j < arrBin[i].length; j = j + 2) {
+            if (arrBin[i][0] === '*') {
+                morse += ' ';
+                break
+            }
+            if (arrBin[i][j] === '1' && arrBin[i][j + 1] === '0') {
+                morse += ".";
+            } else if (arrBin[i][j] === '1' && arrBin[i][j + 1] === '1') {
+                morse += "-";
+            }
+        }
+
+        res += ((morse === ' ') ? ' ' : MORSE_TABLE[morse])
+    }
+    
+    console.log(res);
+    return res
 }
 
 module.exports = {
